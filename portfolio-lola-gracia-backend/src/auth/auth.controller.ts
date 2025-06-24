ec7f93f70eaa7a4d07fbd3ff81c2d14317ef555c
gmarcogracia <gmarcogracia2004@gmail.com>
2025-06-24 19:13:20 +0200

    Se ha restaurado la nomenclatura userId

--- portfolio-lola-gracia-backend/src/auth/auth.controller.ts
+++ portfolio-lola-gracia-backend/src/auth/auth.controller.ts
@@ -5,39 +5,48 @@ import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){
+    constructor(private authService: AuthService) {
 
     }
 
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    async login(@Body() input:{username:string,password:string},
-         @Res({ passthrough: true }) res: Response
-        ){
-        const  userAccessTokenAndStuff = await  this.authService.authenticate(input);
-        if(userAccessTokenAndStuff){
-    res.cookie('access_token', userAccessTokenAndStuff.accessToken, {
-    httpOnly: true,      
-    secure: false,        //TODO poner a true cuando se pase a https
-    sameSite: 'lax', //Porque usan dominios diferentes (Cambiar cuando se suba a servidor)
-    maxAge: 1000 * 60 * 24*60 // 1 dia de validez
-  });
-        
-  //La access token no se devuelve por seguridad
- delete (userAccessTokenAndStuff as {accessToken?: string}).accessToken;
+    async login(@Body() input: { username: string, password: string },
+        @Res({ passthrough: true }) res: Response
+    ) {
+        const userAccessTokenAndStuff = await this.authService.authenticate(input);
+        if (userAccessTokenAndStuff && process.env.ENV=="LOCAL") {
+            res.cookie('access_token', userAccessTokenAndStuff.accessToken, {
+                httpOnly: true,
+                secure: false,        //TODO poner a true cuando se pase a https
+                sameSite: 'lax', //Porque usan dominios diferentes (Cambiar cuando se suba a servidor)
+                maxAge: 1000 * 60 * 24 * 60 // 1 dia de validez
+            })
+
+            //La access token no se devuelve por seguridad
+            delete (userAccessTokenAndStuff as { accessToken?: string }).accessToken;
 
 
-        
-        return userAccessTokenAndStuff;
+
+            return userAccessTokenAndStuff;
+        } else if (userAccessTokenAndStuff && process.env.ENV!="LOCAL") {
+            //Configuracion de cookies en produccion (Si no no se guardan)
+            res.cookie('access_token', userAccessTokenAndStuff.accessToken, {
+                httpOnly: true,
+                secure:true,
+                sameSite: 'none', 
+                maxAge: 1000 * 60 * 60 * 24,
+            });
+
         }
         return null;
- 
+
     }
-//Ruta para recibir info solo del usuario, Esta protegida mediante un guard
-@UseGuards(AuthGuard)
+    //Ruta para recibir info solo del usuario, Esta protegida mediante un guard
+    @UseGuards(AuthGuard)
     @Get('me')
-    getUserInfo( @Request() request){
+    getUserInfo(@Request() request) {
         return request.user;
-        
+
     }
 }
