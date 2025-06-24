917780d1a2e9d6ecaa875932b63b51b8f8125daf
gmarcogracia <gmarcogracia2004@gmail.com>
2025-06-24 20:12:04 +0200

    ELiminada una referencia a localhost en app listen

--- portfolio-lola-gracia-backend/src/main.ts
+++ portfolio-lola-gracia-backend/src/main.ts
@@ -5,14 +5,14 @@ import cookieParser  from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const host ='localhost'
-//Esto es necesario porque si no saltan fallos de cors al hacer peticiones
+
+  //Esto es necesario porque si no saltan fallos de cors al hacer peticiones
   app.enableCors({
     origin: process.env.FRONTEND_SERVER_ADDRESS, // Next.js frontend
     credentials: true, // Para usar cookies y headers
   });
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3001,host); //CAMBIAR PUERTO
+  await app.listen(process.env.PORT ?? 3001); //CAMBIAR PUERTO
   const logger = new Logger('bootstrap');
   logger.log(`Listening on ${await app.getUrl()}`);
 }
