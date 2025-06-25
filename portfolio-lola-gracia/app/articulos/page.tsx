33617854c869c0edc1c16071d371381ac2f3eb0f
gmarcogracia <gmarcogracia2004@gmail.com>
2025-06-25 19:23:34 +0200

    Se ha refactorizado a sintaxis con use state y use effect

--- portfolio-lola-gracia/app/articulos/page.tsx
+++ portfolio-lola-gracia/app/articulos/page.tsx
@@ -1,27 +1,29 @@
-//Se ha pasado a cliente en el ultimo mome to porque desplegar aplicaciones es una mierda
 'use client';
 
+import { useEffect, useState } from 'react';
 import ArticleList from './components/articleList';
 import { getUserFromCookie } from '../functions/functions';
 import { Button } from '@mantine/core';
 import Link from 'next/link';
 
-      
-export default async function ArticulosPage() {
-const role =await getUserFromCookie();
+export default function ArticulosPage() {
+  const [role, setRole] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      const roleFromCookie = await getUserFromCookie();
+      setRole(roleFromCookie ?? undefined);
+    };
+    fetchRole();
+  }, []);
+
   return (
-    <main >
+    <main>
       <ArticleList role={role ?? undefined} />
-        {( role && role<= 2 ) && (
+      {role && role <= 2 && (
         <div style={{ marginBottom: '1rem' }}>
-              <Link href={'/articulos/nuevo-articulo'}>
-              {/* Se ha comentado  una linea que hacia que se pasase el rol encriptado junto a una clave secrita mediante la url
-                 ?'+ btoa("role")  + "=$" +(btoa(role))
-                
-                +"&"+ btoa(encodeURIComponent("claveSecreta=223456")) } state={{role:role}} */}
-<Button color="teal" >
-            Crear nuevo artículo
-          </Button>
+          <Link href="/articulos/nuevo-articulo">
+            <Button color="teal">Crear nuevo artículo</Button>
           </Link>
         </div>
       )}
