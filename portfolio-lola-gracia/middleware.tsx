c62996c4fd956113f487f68815199cba1921680e
gmarcogracia <gmarcogracia2004@gmail.com>
2025-06-25 20:05:05 +0200

    Retoques middleware Nuevo

--- portfolio-lola-gracia/middleware.tsx
+++ portfolio-lola-gracia/middleware.tsx
@@ -1,24 +1,29 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getUserFromCookie } from './app/functions/functions';
 //Se usa jose (Nombre gracioso, i know porque el middleware es incompatible con jsonWebtoken)
-import { jwtVerify } from 'jose';
+// import { jwtVerify } from 'jose';
 
-async function getRoleFromToken(token: string | undefined): Promise<number | null> {
-  if (!token) return null;
+//Esto ya no se necesita porque  ahora se hace desde elbackend
+// async function getRoleFromToken(token: string | undefined): Promise<number | null> {
+//   if (!token) return null;
 
-  try {
-    const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
-    const { payload } = await jwtVerify(token, secret);
-    return payload.roleId as number;
-  } catch (err) {
-    console.error("JWT inválido:", err);
-    return null;
-  }
-}
+//   try {
+//     const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
+//     const { payload } = await jwtVerify(token, secret);
+//     return payload.roleId as number;
+//   } catch (err) {
+//     console.error("JWT inválido:", err);
+//     return null;
+//   }
+// }
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('access_token')?.value;
-  const role = await getRoleFromToken(token);
+  // const token = request.cookies.get('access_token')?.value;
+  const role = await getUserFromCookie();
+  console.log("ROleMiddleware",role);
+  console.log("Cookies")
+  console.log(request.cookies)
 
 
 
